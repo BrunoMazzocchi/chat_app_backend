42a517efd6b705e09813c6c3367027884608bf0f
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2023-06-25 18:45:27 -0600

    Token generated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,15 @@
 const { response } = require('express');
-const User  = require('../models/user');
+const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const { generateJWT } = require('../helpers/jwt');
-const createUser = async (req, res = response ) => {
+
+const createUser = async (req, res = response) => {
 
     const { email, nickname } = req.body;
 
     try {
         const emailExists = await User.findOne({ email });
-        if ( emailExists ) {
+        if (emailExists) {
             return res.status(400).json({
                 ok: false,
                 msg: 'Email already exists.'
@@ -16,31 +17,31 @@ const createUser = async (req, res = response ) => {
         }
 
         const nicknameExists = await User.findOne({ nickname });
-        if ( nicknameExists ) {
+        if (nicknameExists) {
             return res.status(400).json({
                 ok: false,
                 msg: 'Nickname already exists.'
             });
         }
-    
-        const user = new  User (req.body);
+
+        const user = new User(req.body);
 
         // Encrypt password
-        
+
         const salt = bcrypt.genSaltSync();
-        user.password = bcrypt.hashSync( user.password, salt );
+        user.password = bcrypt.hashSync(user.password, salt);
 
         // Save user
-        await  user.save();
+        await user.save();
 
         // Generate JWT
-        const token = await generateJWT( user.id );
-         
-    res.json({
-        ok: true,
-        user, 
-        token
-    });
+        const token = await generateJWT(user.id);
+
+        res.json({
+            ok: true,
+            user,
+            token
+        });
     }
     catch (error) {
         console.log(error);
@@ -53,9 +54,51 @@ const createUser = async (req, res = response ) => {
 
 
 
-    
+
 };
 
+const login = async (req, res = response) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User does not exist.'
+            });
+        }
+
+        // Confirm passwords
+        const validPassword = bcrypt.compareSync(password, user.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password is not valid.'
+            });
+        }
+
+
+        // Generate JWT
+        const token = await generateJWT(user.id);
+
+
+        res.json({
+            ok: true,
+
+            user,
+            token
+        });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Unable to login. Please contact the administrator'
+        });
+    }
+}
+
 module.exports = {
-    createUser
+    createUser,
+    login
 }
--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const router = Router();
-const { createUser } = require('../controllers/auth');
+const { createUser, login } = require('../controllers/auth');
 const { validateFields } = require('../middlewares/validate_fields');
 
 router.post('/new', [
@@ -17,11 +17,12 @@ router.post('/new', [
     check('email', 'Email is required').isEmail(),
     check('password', 'Password is required').not().isEmpty(),
     check('password', 'Password must be at least 6 characters').isLength({ min: 6 }),
-    check('password', 'Password must include at least 1 character or number').matches(/^(?=.*\d)(?=.*[a-z])[0-9a-z]{6,}$/i), 
+    check('password', 'Password must include at least 1 character or number').matches(/^(?=.*\d)(?=.*[a-z])[0-9a-z]{6,}$/i),
     check('nickname', 'Nickname is required').not().isEmpty(),
     validateFields
 
 ], createUser);
 
+router.post('/', login);
 
 module.exports = router;
